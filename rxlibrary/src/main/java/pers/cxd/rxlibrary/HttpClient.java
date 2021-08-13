e8a2f2f6575fe1237bfbcdf4240a1a3f28467805
caoxuedong <spdcard@Gmail.com>
2021-08-13 15:01:38 +0800

    feat: make HttpClient support multi api interface

--- rxlibrary/src/main/java/pers/cxd/rxlibrary/HttpClient.java
+++ rxlibrary/src/main/java/pers/cxd/rxlibrary/HttpClient.java
@@ -1,29 +1,37 @@
 package pers.cxd.rxlibrary;
 
-import java.lang.reflect.ParameterizedType;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 
-public abstract class HttpClient<I> {
+public abstract class HttpClient {
 
-    private I mApi;
+    private final Retrofit mRetrofitClient;
+    private final Map<Class<?>, Object> mApiMap = new ConcurrentHashMap<>();
 
-    public I getApi(){
-        return mApi;
+    protected HttpClient(){
+        mRetrofitClient = createRetrofitClient();
+    }
+
+    public <I> I getApi(Class<I> apiClass) {
+        return (I) mApiMap.computeIfAbsent(apiClass, new Function<Class<?>, Object>() {
+            @Override
+            public Object apply(Class<?> aClass) {
+                return mRetrofitClient.create(aClass);
+            }
+        });
     }
 
     protected abstract String getBaseUrl();
     protected abstract Converter.Factory[] getConvertFactories();
     protected abstract OkHttpClient createHttpClient();
 
-    protected HttpClient(){
-        createRetrofitClient();
-    }
-
-    private void createRetrofitClient(){
+    private Retrofit createRetrofitClient(){
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(getBaseUrl())
                 .client(createHttpClient())
@@ -31,7 +39,7 @@ public abstract class HttpClient<I> {
         for (Converter.Factory factory : getConvertFactories()){
             builder.addConverterFactory(factory);
         }
-        mApi = builder.build().create((Class<I>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+        return builder.build();
     }
 
 }
