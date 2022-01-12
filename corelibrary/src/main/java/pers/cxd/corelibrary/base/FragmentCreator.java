92a1c7db252118f596dde5d9d20eee89ad20252f
caoxuedong <spdcard@gmail.com>
2022-01-12 10:33:21 +0800

    docs: update FragmentCreator doc

--- corelibrary/src/main/java/pers/cxd/corelibrary/base/FragmentCreator.java
+++ corelibrary/src/main/java/pers/cxd/corelibrary/base/FragmentCreator.java
@@ -2,6 +2,13 @@ package pers.cxd.corelibrary.base;
 
 import androidx.fragment.app.Fragment;
 
+/**
+ * A Creator to create fragment by {@code fmtClazz}
+ *
+ * @param <T> Fragment type
+ * @since 1.0.5
+ * @author pslilysm
+ */
 public interface FragmentCreator<T extends Fragment> {
 
     T create(Class<T> fmtClazz);
